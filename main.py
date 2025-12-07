33f9ff658494618860d84c0249c9e4599cd0ea4c
Mahmoud <mahmoudebr25@gmail.com>
2025-12-07 12:19:20 +0200

    generator function: single function with optional sorting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-from utils.generator import generate_sorted_list
+from utils.generator import generate_random_list
 from src.listBased import binary_search, linear_search
 import time
 
@@ -9,7 +9,7 @@ def main():
     # 1. Generate Data
     dataset_size = 100000
     print(f"Generating sorted dataset of {dataset_size} items...")
-    data = generate_sorted_list(dataset_size, 0, 200000)
+    data = generate_random_list(dataset_size, 0, 200000, sorted=True)
     target = data[-1] # Pick the last element to force worst-case for Linear Search
     
     print(f"Targeting value: {target}")
